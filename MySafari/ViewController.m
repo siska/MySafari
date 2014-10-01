6fb5221295a651ca91c0211c95eb97bba07cefe4
Nathan Hosselton <nathanhosselton@gmail.com>
2014-10-01 14:32:33 -0500

    Finished step 10

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 
 @end
 
@@ -19,11 +21,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
+    [self loadWebPageWithString:@"http://www.google.com"];
 }
 
 - (void)webViewDidStartLoad:(UIWebView *)webView {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+
+    if (self.webView.canGoBack) {
+        self.backButton.hidden = NO;
+    } else {
+        self.backButton.hidden = YES;
+    }
+
+    if (self.webView.canGoForward) {
+        self.forwardButton.hidden = NO;
+    } else {
+        self.forwardButton.hidden = YES;
+    }
+
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
@@ -32,11 +48,15 @@
 
 //the below method just hides the keyboard once the user hits enter. we're coopting it to perform other actions because it's convenient for us
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    NSURL *url = [NSURL URLWithString:self.urlTextField.text];
+    [self loadWebPageWithString:textField.text];
+
+    return YES;
+}
+
+- (void)loadWebPageWithString:(NSString *)urlString {
+    NSURL *url = [NSURL URLWithString:urlString];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:urlRequest];
-    
-    return YES;
 }
 
 - (IBAction)onBackButtonPressed:(id)sender {
